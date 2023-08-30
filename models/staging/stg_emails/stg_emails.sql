fc83994d88fa5fd80375e14248be547c24ea1fc1
brspartzcb@gmail.com <brochill@Brspartz-MacBook-Pro.local>
2023-08-30 22:18:04 +0100

    fixing stg_emails SQL formatting & YML file

---
+++ models/staging/stg_emails/stg_emails.sql
@@ -0,0 +1,28 @@
+{{
+  config (
+      materialized='view',
+      unique_key='email_id',
+      cluster_by=['sent_date']
+      )
+}}
+
+WITH source AS (
+
+  SELECT * FROM {{ ref('emails') }}
+
+),
+  
+renamed AS (
+
+SELECT
+
+  e.id AS email_id,
+  e.customer_id,
+  e.sent_date,
+  e.subject,
+  e.opened,
+  e.clicked
+
+)
+
+SELECT * FROM renamed
