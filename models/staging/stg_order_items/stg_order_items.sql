09553158786063fba9e3c04cb6e1f265fadebaa2
brspartzcb@gmail.com <brochill@Brspartz-MacBook-Pro.local>
2023-08-30 22:39:39 +0100

    revising stg_order_items sql formatting

--- models/staging/stg_order_items/stg_order_items.sql
+++ models/staging/stg_order_items/stg_order_items.sql
@@ -5,11 +5,23 @@
       )
 }}
 
-SELECT
-    id AS order_item_id,
-    created_at,
-    updated_at,
-    order_id,
-    product_id,
-    quantity
-FROM {{ ref('order_items') }}
+WITH source AS (
+
+  SELECT * FROM {{ ref('order_items') }}
+
+),
+
+renamed as (
+
+    SELECT
+        id AS order_item_id,
+        created_at,
+        updated_at,
+        order_id,
+        product_id,
+        quantity
+    FROM source AS p
+
+)
+
+SELECT * FROM renamed
